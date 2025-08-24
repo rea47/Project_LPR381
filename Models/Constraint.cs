2bafaab905c2e19d8eafd73789ed72e6935dd60a
Reagile Motsepe <reagilemotsepe@gmail.com>
2025-08-24 18:07:59 +0200

    Added code for Constraints and added 2 new samples for input

--- Models/Constraint.cs
+++ Models/Constraint.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Project_LPR31.Models
 {
-    internal class Constraint
+    /// Enumeration for optimization direction
+    public enum ObjectiveType
     {
+        Maximize,
+        Minimize
+    }
+
+    /// Enumeration for constraint relations
+    public enum ConstraintRelation
+    {
+        LessOrEqual,
+        GreaterOrEqual,
+        Equal
+    }
+
+    /// Enumeration for variable sign restrictions
+    public enum SignRestriction
+    {
+        NonNegative,    // +
+        NonPositive,    // -
+        Unrestricted,   // urs
+        Integer,        // int
+        Binary          // bin
+    }
+
+    /// Enumeration for solution status (for future solver integration)
+    public enum SolutionStatus
+    {
+        Optimal,
+        Infeasible,
+        Unbounded,
+        Unknown
     }
 }
